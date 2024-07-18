cdcea12a16ee0bc1861c945fea80d098877c49d2
Dhea Marenty Suwarno <dheamarentys@gmail.com>
2024-07-18 21:58:06 +0700

    first assignment

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const form = document.getElementById('bmi-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Mencegah submit form default
+
+  const tinggi = parseFloat(document.getElementById('tinggi').value);
+  const berat = parseFloat(document.getElementById('berat').value);
+
+  // Hitung BMI (rumus BMI = berat / (tinggi/100)^2)
+  const bmi = berat / Math.pow(tinggi / 100, 2);
+
+  // Panggil fungsi untuk menampilkan hasil BMI
+  tampilkanHasilBMI(bmi);
+});
 function tampilkanHasilBMI(bmi) {
     const hasilContainer = document.getElementById('hasil-container');
     hasilContainer.innerHTML = ""; // Kosongkan container sebelum menampilkan hasil baru
@@ -31,5 +45,4 @@ function tampilkanHasilBMI(bmi) {
     hasilContainer.innerHTML += `<p>BMI: ${bmi.toFixed(2)}</p>`;
     hasilContainer.innerHTML += `<p>Kategori: ${kategori}</p>`;
     hasilContainer.innerHTML += `<p>${pesan}</p>`;
-  }
-  
+}
